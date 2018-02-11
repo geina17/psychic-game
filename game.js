d59a13e04a04167704ab72236c0df071ef589cda
geina17 <33171782+geina17@users.noreply.github.com>
2018-02-10 19:48:00 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,79 +1,59 @@
-var keyBoard = ["a","z","s","d","f","g","h","j","k","l","q","w","e","r","t","y","u","i","o","p","x","c","v","b","n","m"]
-		console.log();
+var keyBoard = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+console.log();
 
-		// var blanks = [];
-		
-		var wins = 0;
-		var losses = 0;
-		var guesses = 3;
-		var guessesLeft = 3;
-		var guessedLetters = [];
-		
-		var left =function(){
-			document.querySelector("#guessesLeft").innerHTML = "Guesses Left" + guessesLeft;
-		};
-		console.log();
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessed = [];
+var toGuess = null;
 
-		var toGuess = function(){
-			letterToGuess =.keyBoard[Math.floor(Math.random() *keyBoard.length];
-		};
-		console.log();
+var computerGuess = keyBoard[Math.floor(Math.random() *keyBoard.length)];
 
-		var guessed = function(){
-			document.querySelector('#picked').innerHTML = "Your Guesses: " + guessed.join(', ');
-		};
-		console.log();
+var updateGuessesLeft = function() {
+  
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
 
-		function reset() {
-			var reset = function() {
-			  totalGuesses =3;
-			  guessesLeft = 3;
-			  guessedLetters = [];
+var updatetoGuess = function() {
+  this.toGuess = this.keyBoard[Math.floor(Math.random() * this.keyBoard.length)];
+};
+var updateGuessesSoFar = function() {
+  
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessed.join(', ');
+};
 
-			  updateLetterToGuess();
-			  updateGuessesLeft();
-			  updateGuessesSoFar();
-			}
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessed = [];
 
-			updateLetterToGuess();
-			updateGuessesLeft();
-		}
-		console.log();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+}
 
-		// function startGame(){
+updateGuessesLeft();
 
-		// 	word = keyBoard[Math.floor(Math.random() *keyBoard.length)];
-		// 	console.log(word);
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-			
-		// 	wordlength = letersinword.length;
-		// 	console.log(wordlength);
+  guessed.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
 
-		// 	for(var i =0; i<wordlength;i++){
-		// 	blanks.push("_");
-		// 	}
-		// }
-		// console.log(blanks);
-	
-		document.onkeyup = function(event) {
-		    guessesLeft--;
-		  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		  guessedLetters.push(userGuess);
-
-		  updateGuessesLeft();
-		  updateGuessesSoFar();
-
-	        if (guessesLeft > 0){
-	            if (userGuess == letterToGuess){
-	                wins++;
-	                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-	                alert("You are psychic!");
-	                reset();
-	            }
-	        }else if(guessesLeft == 0){
-	            losses++;
-	            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-	            alert("You're not psychic, maybe try again?");
-	            reset();
-	        }
-         };   
+    if (guessesLeft > 0){
+        if (userGuess == toGuess){
+            wins++;
+            document.querySelector('#wins').innerHTML = "Wins: " + wins;
+            alert("You are psychic!");
+            reset();
+        }
+        }else if(guessesLeft == 0){ 
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("You are not psychic, try again?");
+       
+            reset();
+        }
+};  
